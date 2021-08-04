5539afdb635898d0b979b2d3edeb2b6e5050cb25
miloszczapla <miloszczapla@gmail.com>
2021-08-04 23:21:12 +0200

    added link in nav bar, drawer to style

--- salon/components/NavBar.tsx
+++ salon/components/NavBar.tsx
@@ -1,16 +1,39 @@
 import { icons } from '../helpclasses/getImageByKey';
 import Image from 'next/image';
 import Drawer from './Drawer';
-import { useState } from 'react';
-import { Collapse } from 'react-collapse';
+import React, { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
 
 //NavBar that is present on every site
 const NavBar = () => {
   const [isDrawer, setIsDrawer] = useState(false);
+  const navRef = useRef(null);
+
+  const handleClick = (e: any) => {
+    //@ts-ignore
+    if (navRef.current && !navRef?.current.contains(e.target)) {
+      setIsDrawer(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
   return (
-    <nav className='flex flex-row justify-between items-center p-2 bg-contrast   dark:bg-contrast-dark transition duration-300 relative'>
+    <nav
+      ref={navRef}
+      className='flex flex-row justify-between items-center p-2 bg-contrast   dark:bg-contrast-dark transition duration-300 relative'
+    >
       {/* logo of the company icon  */}
-      <Image src={icons.hairicon} alt='firma' className='pl-4 h-12' />
+      <Image
+        src={icons.hairicon}
+        alt='AM Dermatrychologia'
+        className='pl-4 h-12'
+      />
       {/* iconify icon  */}
       <button
         className='md:hidden'
@@ -23,9 +46,36 @@ const NavBar = () => {
           data-icon={icons.hamburger}
         ></span>
       </button>
-      <Drawer isDrawer={isDrawer} />
+      <Drawer isDrawer={isDrawer}>
+        <NavBlock path='/login'>konto</NavBlock>
+        <NavBlock path='/offer'>oferta</NavBlock>
+        <NavBlock path='tel:+48123456789'>umów się</NavBlock>
+        <NavBlock path='/'>Home</NavBlock>
+      </Drawer>
+
+      <div className='hidden md:flex gap-3'>
+        <NavBlock path='/login'>konto</NavBlock>
+        <NavBlock path='/offer'>oferta</NavBlock>
+        <NavBlock path='tel:+48123456789'>umów się</NavBlock>
+        <NavBlock path='/'>Home</NavBlock>
+      </div>
     </nav>
   );
 };
 
 export default NavBar;
+
+interface NavBlockProps {
+  path: string;
+  children: any;
+}
+
+const NavBlock = ({ children, path }: NavBlockProps) => {
+  return (
+    <Link href={path}>
+      <div className='bg-secondarymain-dark bg-opacity-70 text-left w-full px-4 py-2 rounded-sm dark:bg-lightBulb dark:text-white dark:bg-opacity-70 cursor-pointer md:bg-transparent md:text-center md:flex md:justify-center md:items-center md:focus:bg-secondarymain md:hover:bg-secondarymain md:hover:bg-opacity-10 md:focus:bg-opacity-10 md:text-2xl md:whitespace-nowrap'>
+        {children}
+      </div>
+    </Link>
+  );
+};
